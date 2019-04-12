c6fb97a74c6176b3538847b32d6899b151f03573
peculiarOne <wayne@campbellscorner.org.uk>
2019-04-12 18:21:46 +0100

    up to error handling

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,6 +23,11 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
+    let qty = match qty {
+        Ok(quantity) => quantity,
+        Err(error) => return Err(error),
+    };
+
     Ok(qty * cost_per_item + processing_fee)
 }
 
--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,8 +5,22 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+macro_rules! my_macro {
+    ($val:expr) => {
+        {
+           let mut greeting = String::from("Hello ");
+           greeting.push_str(stringify!($val));
+           greeting
+        }
+    } 
+}
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
+    }
+
+    if my_macro!(56) != "Hello 56" {
+        panic!("Wrong output")
     }
 }
