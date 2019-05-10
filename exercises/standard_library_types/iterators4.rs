95c6e5220bce9c48aeba929676871e75056eff82
peculiarOne <wayne@campbellscorner.org.uk>
2019-05-10 17:48:50 +0100

    finished iterators4

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,7 @@
 // iterators4.rs
 
-pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return factorial of num
+pub fn factorial_rec(num: u64) -> u64 {
+    // Complete this function to return factorial_rec of num
     // Do not use:
     // - return
     // For extra fun don't use:
@@ -10,6 +10,18 @@ pub fn factorial(num: u64) -> u64 {
     // For the most fun don't use:
     // - recursion
     // Scroll down for hints.
+
+    fn inner_fact(num: u64, acc: u64) -> u64 {
+        match num {
+            0 => acc,
+            _ => inner_fact(num -1, num * acc)
+        }
+    }
+    inner_fact(num, 1)
+}
+
+pub fn factorial(num: u64) -> u64 {
+    (1..=num).product()
 }
 
 #[cfg(test)]
@@ -18,15 +30,18 @@ mod tests {
 
     #[test]
     fn factorial_of_1() {
+        assert_eq!(1, factorial_rec(1));
         assert_eq!(1, factorial(1));
     }
     #[test]
     fn factorial_of_2() {
+        assert_eq!(2, factorial_rec(2));
         assert_eq!(2, factorial(2));
     }
 
     #[test]
     fn factorial_of_4() {
+        assert_eq!(24, factorial_rec(4));
         assert_eq!(24, factorial(4));
     }
 }
